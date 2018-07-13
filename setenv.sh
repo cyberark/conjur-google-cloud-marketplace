ab36b575946e3ced1446ca2d1b9a6e2ff70fd4a4
Dustin Collins <dustin.collins@cyberark.com>
2018-07-13 10:09:40 -0500

    Remove unneeded env vars

--- setenv.sh
+++ setenv.sh
@@ -5,8 +5,3 @@ export NAMESPACE="$(whoami)"
 
 export REGISTRY=us.gcr.io/conjur-cloud-launcher-onboard
 
-export DEPLOYER_SERVICEACCOUNT_NAME=$APP_NAME-deployer-sa
-export DEPLOYER_IMAGE=$REGISTRY/cyberark/conjur-deployer
-export DEPLOYER_CONFIGMAP_NAME=$APP_NAME-deployer-configmap
-
-export PARAMETERS=schema.yaml
